1705563dc836b87ab20d1aa1d8a9982aa31b3d80
jchyang <1214058051@qq.com>
2020-10-09 11:01:48 +0800

    test

--- src/com/jchyang/test/Main.java
+++ src/com/jchyang/test/Main.java
@@ -3,6 +3,7 @@ package com.jchyang.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld-test");
+
     }
 }
